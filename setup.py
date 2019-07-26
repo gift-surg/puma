398d68c06be38d0b5c9362fa0c626c58da8cb305
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-07-26 14:13:56 +0100

    Get long description off README

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
+from io import open
 
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='puma',
     description='Python Unified Multi-tasking API (PUMA)',
-    long_description='Python Unified Multi-tasking API (PUMA)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 
     url='https://github.com/gift-surg/puma',
     author='GIFT-Surg Consortium',
