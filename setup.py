31051fe22f4b325722cd6ecd9cfaecf1eba28114
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-07-26 11:52:30 +0100

    Skeleton to build upon

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name='puma',
+    description='Python Unified Multi-tasking API',
+
+    url='https://github.com/gift-surg/puma',
+    author='GIFT-Surg Consortium',
+
+    packages=[
+        'puma',
+    ],
+)
