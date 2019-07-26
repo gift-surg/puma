0a665895e290e27684a4e034ca1f24241bbd1713
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-07-26 12:19:31 +0100

    Add long description

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 
 setup(
     name='puma',
-    description='Python Unified Multi-tasking API',
+    description='Python Unified Multi-tasking API (PUMA)',
+    long_description='Python Unified Multi-tasking API (PUMA)',
 
     url='https://github.com/gift-surg/puma',
     author='GIFT-Surg Consortium',
